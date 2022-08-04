f74d52be9bd4fe28381f5e78195e87c9aecbf709
Gjoll <KurtA@Gjoll.com>
2022-08-04 15:30:04 -0500

    .

--- build/Build.cs
+++ build/Build.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Linq;
 using Nuke.Common;
 using Nuke.Common.CI;
 using Nuke.Common.Execution;
@@ -10,33 +8,29 @@ using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Tools.GitVersion;
 using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
-[CheckBuildProjectConfigurations]
 [ShutdownDotNetAfterServerBuild]
 class Build : NukeBuild
 {
-    /// Support plugins are available for:
-    ///   - JetBrains ReSharper        https://nuke.build/resharper
-    ///   - JetBrains Rider            https://nuke.build/rider
-    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
-    ///   - Microsoft VSCode           https://nuke.build/vscode
 
-    public static int Main () => Execute<Build>(x => x.Compile);
+    public static int Main() => Execute<Build>(x => x.All);
 
-    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
-    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+    //[Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    //readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
     [Solution] readonly Solution Solution;
-    [GitRepository] readonly GitRepository GitRepository;
-    [GitVersion] readonly GitVersion GitVersion;
+    //[GitRepository] readonly GitRepository GitRepository;
+    //[GitVersion] readonly GitVersion GitVersion;
 
-    AbsolutePath SourceDirectory => RootDirectory / "source";
+    Project ServiceProject => Solution.GetProject("RadElement2Fhir");
+
+    AbsolutePath SourceDirectory => RootDirectory / "Projects";
     AbsolutePath TestsDirectory => RootDirectory / "tests";
     AbsolutePath OutputDirectory => RootDirectory / "output";
+
 
     Target Clean => _ => _
         .Before(Restore)
@@ -45,26 +39,74 @@ class Build : NukeBuild
             SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
             TestsDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
             EnsureCleanDirectory(OutputDirectory);
+
+            if (ServiceProject == null)
+                throw new System.Exception($"ServiceProject not found");
         });
 
     Target Restore => _ => _
+         .Unlisted()
         .Executes(() =>
         {
             DotNetRestore(s => s
                 .SetProjectFile(Solution));
         });
 
-    Target Compile => _ => _
-        .DependsOn(Restore)
+
+    Target CompileService => _ => _
+         .Unlisted()
+        .DependsOn(Clean)
         .Executes(() =>
         {
-            DotNetBuild(s => s
-                .SetProjectFile(Solution)
-                .SetConfiguration(Configuration)
-                .SetAssemblyVersion(GitVersion.AssemblySemVer)
-                .SetFileVersion(GitVersion.AssemblySemFileVer)
-                .SetInformationalVersion(GitVersion.InformationalVersion)
-                .EnableNoRestore());
+            DotNetPublish(s => s
+                .SetConfiguration(Configuration.Release)
+                //.SetAssemblyVersion(GitVersion.AssemblySemVer)
+                //.SetFileVersion(GitVersion.AssemblySemFileVer)
+                //.SetInformationalVersion(GitVersion.AssemblySemFileVer)
+                //.SetDescription(GitVersion.InformationalVersion)
+                .SetSelfContained(false)
+                .SetProject(ServiceProject)
+                .SetOutput(OutputDirectory / ServiceProject.Name)
+            );
         });
 
+
+    Target Compile => _ => _
+        .DependsOn(Clean)
+        .DependsOn(CompileService)
+        ;
+
+    Target Push => _ => _
+        .DependsOn(Pack)
+        .Executes(() =>
+        {
+            DotNetNuGetPush(s => s
+                .SetSource("github")
+                .CombineWith(OutputDirectory.GlobFiles("*.nupkg"), (s, p) => s.SetTargetPath(p))
+            );
+        });
+
+    Target Pack => _ => _
+        .DependsOn(Clean)
+        .DependsOn(Compile)
+        .Before(Push)
+        .Executes(() =>
+        {
+            DotNetPack(s => s
+                .SetProject(ServiceProject)
+                .SetConfiguration(Configuration.Release)
+                //.SetAssemblyVersion(GitVersion.AssemblySemVer)
+                //.SetFileVersion(GitVersion.AssemblySemFileVer)
+                //.SetInformationalVersion(GitVersion.AssemblySemFileVer)
+                //.SetDescription(GitVersion.InformationalVersion)
+                //.SetProperty("PackageVersion", GitVersion.NuGetVersionV2)
+                .SetOutputDirectory(OutputDirectory)
+            );
+        });
+    
+    Target All => _ => _
+        .DependsOn(Clean)
+        .DependsOn(Compile)
+        .DependsOn(Pack)
+        ;
 }
